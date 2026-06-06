b4d79b349eff5bfadcf5fb689576f718cdfa6509
Sanya <sanya.24bce10129@vitbhopal.ac.in>
2026-06-06 20:05:06 +0530

    Created homepage FitTrack

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>🏋️ FitTrack</h1>
+
+      <h2>Today's Workout: Leg Day</h2>
+
+      <h3>Daily Goals</h3>
+
+      <ul>
+        <li>☐ Complete Workout</li>
+        <li>☐ Consume 200g Protein</li>
+        <li>☐ Drink 3L Water</li>
+        <li>☐ Sleep 8 Hours</li>
+      </ul>
+
+      <h3>Current Streak 🔥</h3>
+      <p>1 Day</p>
+    </div>
+  );
+}
+
+export default App;
